578e64b7e4967925f4275b9bdc31492a940e9aac
roadWithDream <elehfut@163.com>
2018-10-29 10:31:46 +0800

    delete

--- testweb/src/com/hundsun/lcpt/algorithm/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hundsun.lcpt.algorithm;
-
-public class Main {
-	public static void  main(String args[]){
-		String ss = DES.enc("111", "AHsAndMoeA");
-		System.out.println(ss);
-		System.out.println(DES.dec(ss, "AHsAndMoeA"));
-	}
-}
